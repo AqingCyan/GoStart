c7962991ee0878e58588bc19356ac9ea8626747b
AqingCyan <aqingcyan@cyanthing.com>
2021-08-26 02:29:40 +0800

    add: 开始学习golang

--- src/ch3/type_test/type_test.go
+++ src/ch3/type_test/type_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 type MyInt int64 // 类型别名
 
-
+// 类型转换
 func TestImplicit(t *testing.T) {
 	var a int32 = 1
 	var b int64
@@ -17,4 +17,21 @@ func TestImplicit(t *testing.T) {
 	var c MyInt
 	c = MyInt(b)
 	t.Log(a, b, c)
+}
+
+// 指针
+func TestPoint(t *testing.T) {
+	a := 1
+	aPoint := &a
+	// aPoint = aPoint + 1  go语言不支持指针运算
+	t.Log(a, aPoint)
+	t.Logf("%T %T", a, aPoint)
+}
+
+// 字符串
+func TestString(t *testing.T) {
+	var s string
+	// s是一个空字符串
+	t.Log("*" + s + "*")
+	t.Log(len(s))
 }
